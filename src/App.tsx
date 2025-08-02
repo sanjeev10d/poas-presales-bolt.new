8f3fd22a1688ac364442010118905dc3e6c1cb24
Sanjeev <sanjeevkumar.v@10decoders.in>
2025-08-02 15:28:17 +0530

    Create fixed sidebar with scrollable menu

--- src/App.tsx
+++ src/App.tsx
@@ -45,8 +45,10 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex">
-      <Sidebar activeModule={activeModule} setActiveModule={setActiveModule} />
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="w-64 lg:w-72 flex-shrink-0">
+        <Sidebar activeModule={activeModule} setActiveModule={setActiveModule} />
+      </div>
+      <div className="flex-1 flex flex-col min-w-0 ml-0">
         <Header activeModule={activeModule} />
         <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto overflow-x-hidden">
           {renderActiveModule()}
