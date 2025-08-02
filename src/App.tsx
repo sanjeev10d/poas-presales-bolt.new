3d009e25b2ff99061c68014d8a2001cb520fcb51
Sanjeev <sanjeevkumar.v@10decoders.in>
2025-08-02 15:28:58 +0530

    Create fixed header with proper spacing

--- src/App.tsx
+++ src/App.tsx
@@ -48,9 +48,9 @@ function App() {
       <div className="w-64 lg:w-72 flex-shrink-0">
         <Sidebar activeModule={activeModule} setActiveModule={setActiveModule} />
       </div>
-      <div className="flex-1 flex flex-col min-w-0 ml-0">
+      <div className="flex-1 flex flex-col min-w-0 pt-24">
         <Header activeModule={activeModule} />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto overflow-x-hidden">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto overflow-x-hidden mt-2">
           {renderActiveModule()}
         </main>
       </div>
