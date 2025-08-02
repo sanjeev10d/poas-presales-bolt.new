ee75a42a6acf530708c87ee755492308d8fbb8d0
Sanjeev <sanjeevkumar.v@10decoders.in>
2025-08-02 15:23:43 +0530

    Fix content cutoff and restore scrolling functionality

--- src/App.tsx
+++ src/App.tsx
@@ -44,11 +44,11 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex overflow-hidden">
+    <div className="min-h-screen bg-gray-50 flex">
       <Sidebar activeModule={activeModule} setActiveModule={setActiveModule} />
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header activeModule={activeModule} />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-auto">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto overflow-x-hidden">
           {renderActiveModule()}
         </main>
       </div>
